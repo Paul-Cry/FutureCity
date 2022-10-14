10440848d1aa172ba911ee358d1ea9e977d105ba
Nikita <74370771+Paul-Cry@users.noreply.github.com>
2022-10-14 12:13:14 +0000

    Add files via upload

--- city.sql
+++ city.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Хост: localhost
--- Время создания: Окт 14 2022 г., 07:46
--- Версия сервера: 10.4.22-MariaDB
--- Версия PHP: 8.1.2
+-- Хост: localhost:3306
+-- Время создания: Окт 14 2022 г., 12:12
+-- Версия сервера: 10.4.21-MariaDB
+-- Версия PHP: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,10 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `requests` (
-  `text` varchar(120) DEFAULT NULL,
+  `content` varchar(120) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `status` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `requests`
+--
+
+INSERT INTO `requests` (`content`, `id_user`, `id`, `status`) VALUES
+('фыавфвыа', 2, 1, 'check'),
+('12132321213321', 1, 2, 'cancel'),
+('444444', 1, 3, 'check'),
+('44444444', 1, 4, 'check'),
+('123132', 2, 5, 'waiting'),
+('asdfasdfafds', 1, 6, ''),
+('Ghbdtmnasdf', 2, 7, 'waiting'),
+('Ghbdtmnasdf', 2, 8, 'waiting'),
+('Как дела тфывафыва', 2, 9, 'waiting');
 
 -- --------------------------------------------------------
 
@@ -46,6 +62,14 @@ CREATE TABLE `users` (
   `password` varchar(120) DEFAULT NULL,
   `role` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'CryptoBear', 'niekita20020802@gmail.com', '$2a$15$/FEQzDYD4zaKqGhk5mLXb.W4a/w/he8h1/VIPZ8zOKP2Z5IUjoDE2', 1),
+(2, 'CryptoBear', '123@123', '$2a$15$Dg1geHzNIFsf8Ick5qOdxODB9x78R7C2omWysz5yhB5d6wCHvhQS.', 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -72,13 +96,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
